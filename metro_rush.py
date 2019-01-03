a2f2e7961632f63b91a0228025d5cb94cc018686
Trân Dương <dtran2108@gmail.com>
2019-01-04 02:51:12 +0700

    successfully parse the map without error handling

--- metro_rush.py
+++ metro_rush.py
@@ -2,17 +2,20 @@ from sys import argv
 
 
 def get_map(filename):
+    """ return a list contains the lists that
+        has the line name and station name and
+        a list that contains the requirements"""
     f = open(filename, 'r')
     content = f.read().splitlines()
     lst = []
     lines = []
-    for element in content:
+    for element in content[:-4]:
         if element.startswith('#') and lst:
             lines.append(lst)
             lst = []
         lst.append(element)
-        print(element)
-    return lines
+    lines.append(lst)
+    return lines, [element for element in content[-3:]]
 
 
 def main():
