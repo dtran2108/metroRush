780176182cb9e7d23bb2d62e6eb19ab11b7812d3
Trân Dương <dtran2108@gmail.com>
2019-01-04 23:32:29 +0700

    Line and Requirement class

---
+++ graph.py
@@ -0,0 +1,16 @@
+class Graph:
+    def __init__(self, map):
+        self.map = map
+
+    def __repr__(self):
+        return str(self.map)
+
+
+class Line:
+    def __init__(self):
+        pass
+
+
+class Requirement:
+    def __init__(self):
+        pass
--- metro_rush.py
+++ metro_rush.py
@@ -1,9 +1,11 @@
 from sys import argv
 from MapParsers import get_map
+from graph import Graph
 
 
 def main():
-    print(get_map(argv[-1]))
+    map = Graph(get_map(argv[-1]))
+    print(map)
 
 
 if __name__ == '__main__':
