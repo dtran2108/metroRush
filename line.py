902d872942820b432c20b89ebcf502f4290a2986
Trân Dương <dtran2108@gmail.com>
2019-01-05 16:27:04 +0700

    get the transfer point

--- line.py
+++ line.py
@@ -6,3 +6,16 @@ class Station:
         """ return the name of the station from 
             its id """
         return self.station_lst[id-1].split(':', 1)[-1]
+
+    def check_transfer(self, id):
+        """ check if the station has a transfer point """
+        station = self.station_lst[id-1]
+        if ":Conn:" in station:
+            return True
+        return False
+
+    def get_transfer(self, id):
+        """ return the line name the station transfers with """
+        station = self.station_lst[id-1]
+        if self.check_transfer(id):
+            return station.split(':Conn')[-1]
