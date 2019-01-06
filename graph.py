1a23213f1d1c4c1e6f619511575423f2580d09af
dtran00 <dtran2108@gmail.com>
2019-01-06 21:00:42 +0700

    path from line to line through transfer point

--- graph.py
+++ graph.py
@@ -19,7 +19,18 @@ class Graph:
         stations = self.get_all_stations()
         for line in stations.keys():
             stations[line] = stations[line].get_all_transfer_points()
-        print(stations)
+        path = [stations[line1][1]]
+        start = path[-1]
+        check = [start[:start.index(':Conn:')]]
+        while start.split(':')[-1].strip() != line2:
+            line_name = start.split(':')[-1].strip()
+            for transfer_point in stations[line_name]:
+                if transfer_point[:transfer_point.index(':Conn:')] not in check:
+                    path.append(transfer_point)
+                    check.append(transfer_point[:transfer_point.index(':Conn:')])
+                    break
+            start = path[-1]
+        print(path)
         pass
 
     def find_path_to_transfer_point(self, station, transfer_point):
