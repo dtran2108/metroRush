2224cdfac07293a3c77003301270939b8d56de13
Trân Dương <dtran2108@gmail.com>
2019-01-13 04:38:57 +0700

    refactor

--- train.py
+++ train.py
@@ -4,6 +4,7 @@ class Train:
         self.current_position = current_position
 
     def check_next_position(self, trains, next_position, destination):
+        """ return True if the next position is valid """
         for train in trains:
             if train.current_position == next_position and train.label != self.label\
                 and next_position != destination:
