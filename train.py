29895d409d08dbceb9891a3e43021988753ead95
Trân Dương <dtran2108@gmail.com>
2019-01-13 04:35:22 +0700

    refactor

--- train.py
+++ train.py
@@ -3,24 +3,26 @@ class Train:
         self.label = 'T' + train_label
         self.current_position = current_position
 
-    def check_next_position(self, path, train_lst, destination):
+    def check_next_position(self, trains, next_position, destination):
+        for train in trains:
+            if train.current_position == next_position and train.label != self.label\
+                and next_position != destination:
+                return True
+        return False
+
+    def find_next_position(self, path, train_lst, destination):
         """ return the next possible position """
         # get the next position
         next_position = path[path.index(self.current_position) + 1]
-        occupied = False
         # check if there is any train at the next position
-        for train in train_lst:
-            # and the next position isn't the destination
-            if train.current_position == next_position and train.label != self.label\
-               and next_position != destination:
-                occupied = True
+        occupied = self.check_next_position(train_lst, next_position, destination)
         if not occupied:
             return next_position
 
     def move(self, path, train_lst, destination):
         """ move the train to the next position """
         # check for the next position
-        new_position = self.check_next_position(path, train_lst, destination)
+        new_position = self.find_next_position(path, train_lst, destination)
         # if there is next position found
         if new_position:
             # move the train to the next position
