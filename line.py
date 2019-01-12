f7aa55823b27f75bffd9348b9b02c698b7dcaa28
Trân Dương <dtran2108@gmail.com>
2019-01-13 04:27:59 +0700

    refactor

--- line.py
+++ line.py
@@ -9,20 +9,16 @@ class Station:
     def get_stationId_from_name(self, name):
         """ return the station's id from its name """
         for station in self.station_lst:
-            if self.check_transfer(station):
-                if station.split(':')[1] == name:
-                    return int(station.split(':')[0])
-            else:
-                if station.split(':')[-1] == name:
-                    return int(station.split(':')[0])
+            if self.check_transfer(station) and station.split(':')[1] == name:
+                return int(station.split(':')[0])
+            elif station.split(':')[-1] == name:
+                return int(station.split(':')[0])
 
     def check_transfer(self, station):
         """ check if the station has a transfer point """
         if ":Conn:" in station:
             return True
-        return False
 
     def get_all_transfer_points(self):
         """ return all the transfer points of a line """
-        return [transfer_point for transfer_point in\
-               self.station_lst if self.check_transfer(transfer_point)]
+        return [transfer_point for transfer_point in self.station_lst if self.check_transfer(transfer_point)]
